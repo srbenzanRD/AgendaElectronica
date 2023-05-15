c690b06244b82b24d322b59be4442648aec9d6f4
Felix Benzan <felixbenz07@gmail.com>
2023-05-15 13:53:15 -0400

    Creacion del modelo o entidad Contacto

---
+++ AgendaElectronica/Data/Models/Contacto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaElectronica.Data.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public string Direccion { get; set; } = null!;
+    }
+}
